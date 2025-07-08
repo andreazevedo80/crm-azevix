1e8a80b234f442a39651afa6d3ac275ad92e1fb4
andreazevedo80 <andre.l.azevedo69@gmail.com>
2025-07-08 13:31:39 -0300

    corrigindo erro data

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime # <-- ADICIONE ESTA LINHA
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -8,13 +9,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     name = db.Column(db.String(150), nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(50), default='vendedor') # Ex: admin, vendedor
+    role = db.Column(db.String(50), default='vendedor')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+# --- MODELOS DE LEAD ---
 
 class Lead(db.Model):
     __tablename__ = 'leads'
